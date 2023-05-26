afa1955b8c0ba97d34094d332040d13e814f8c71
IsanFaizun <113765539+IsanFaizun@users.noreply.github.com>
2023-05-26 16:45:47 +0700

    Ngicil

---
+++ Latihan php.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello world";
+    
+    // One line comment
+    /*
+    this is
+    multiple lines
+    */
+
+    $x = 5;
+    $y = 4;
+    echo $x + $y;
+    ?>
+    
+</body>
+</html>
---
+++ Nomor1.php
@@ -0,0 +1,14 @@
+<?php
+
+$nama = "isan";
+$tanggal = date("d");
+$lenNama = strlen($nama);
+
+if ($tanggal - 2 == $lenNama) {
+    echo "berhasil";
+} elseif ($tanggal == $lenNama) {
+    echo "sedikit lagi";
+} else {
+    echo "coba lagi";
+}
+?>
